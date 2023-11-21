61746a53484905897db270dbf24a6e9fc34b1ead
Kamaruzzaman Zuel <kamaru.zuel@gmail.com>
2023-11-21 17:52:52 +0600

    WIP

--- code/src/Root.tsx
+++ code/src/Root.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import RouterButton from './advanced-buttons/RouterButton'
 import { Outlet } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const Root = () => {
   const [count, setCount] = useState(0);
@@ -15,15 +16,24 @@ const Root = () => {
         </button>
       </div>
 
-      <div>
-        <RouterButton to='/ag-grid'>
+      <div className='flex justify-center gap-5 pb-5'>
+        <RouterButton to='/ag-grid' variant='outlined'>
           AG Grid
         </RouterButton>
-        <RouterButton to='/mui-x-grid'>
+        <RouterButton to='/mui-x-grid' target="_blank" className='hover:text-black hover:!bg-white'>
           MUI X Grid
         </RouterButton>
         <RouterButton to='/kendo-grid'>
           Kendo Grid
+        </RouterButton>
+        <Button >
+          Home 1
+        </Button>
+        <Button component="a" href="/">
+          Home 2
+        </Button>
+        <RouterButton onClick={() => alert("test")}>
+          Home 3
         </RouterButton>
       </div>
 
--- code/src/advanced-buttons/RouterButton.tsx
+++ code/src/advanced-buttons/RouterButton.tsx
@@ -1,21 +1,94 @@
-import { Button } from '@mui/material';
-import { ReactNode } from 'react';
+import { ReactNode, ComponentProps, HTMLAttributeAnchorTarget } from 'react';
+import { Button, ButtonOwnProps, ButtonProps, ButtonTypeMap, ExtendButtonBase } from '@mui/material';
 import { Link } from 'react-router-dom';
+
 type TypeProps = {
-  to: string;
   children: ReactNode;
-};
+  to?: string;
+  target?: HTMLAttributeAnchorTarget;
+} & Omit<ButtonProps, "children" | "to" | "href" | "target">;
 
 const RouterButton = ({
+  children,
   to,
-  children
+  target = "_self",
+  ...rest
 }: TypeProps) => {
-  return (
-    // <div>RouterButton</div>
-    <Button component={Link} to={to}>
+  const defaultButtonProps: ButtonOwnProps = {
+    variant: "contained",
+  };
+
+  if (to != null) {
+    if (target === "_self") {
+      return <LinkTagButton
+        to={to}
+        {...defaultButtonProps}
+        {...rest as ExtendButtonBase<ButtonTypeMap<{}, typeof Link>>}
+      >
+        {children}
+      </LinkTagButton>;
+    } else {
+      return <ATagButton
+        to={to}
+        target={target}
+        {...defaultButtonProps}
+        {...rest as ExtendButtonBase<ButtonTypeMap<{}, "a">>}
+      >
+        {children}
+      </ATagButton>;
+    }
+  } else {
+    return <Button
+      {...defaultButtonProps}
+      {...rest}
+    >
       {children}
-    </Button>
-  )
-}
+    </Button>;
+
+  }
+};
+
+type TypeLinkTagButtonProps = {
+  children: ReactNode;
+  to: string;
+  state?: Record<string, any>;
+} & Omit<ExtendButtonBase<ButtonTypeMap<{}, typeof Link>>, "children" | "component" | "to" | "href" | "target" | "state">;
+
+const LinkTagButton = ({
+  children,
+  to,
+  state,
+  ...rest
+}: TypeLinkTagButtonProps) => {
+  return <Button
+    component={Link}
+    to={to}
+    state={state}
+    {...rest}
+  >
+    {children}
+  </Button>
+};
+
+type TypeATagButtonProps = {
+  children: ReactNode;
+  to: string;
+  target: HTMLAttributeAnchorTarget;
+} & Omit<ComponentProps<ExtendButtonBase<ButtonTypeMap<{}, "a">>>, "children" | "component" | "href" | "target">;
+
+const ATagButton = ({
+  children,
+  to,
+  target,
+  ...rest
+}: TypeATagButtonProps) => {
+  return <Button
+    href={to}
+    target={target}
+    {...rest}
+  >
+    {children}
+  </Button>
+};
 
 export default RouterButton
